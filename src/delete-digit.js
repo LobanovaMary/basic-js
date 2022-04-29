e6ce856eaa555b61e2578280035532550d8de590
Lobanova Maria <lobanovama1@gmail.com>
2022-04-29 23:46:00 +0300

    feat: solve dream-team.js

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const strArr = String(n).split('');
+
+  for (let i = 0; i < 9; i++) {
+    const str = String(i);
+    if (strArr.includes(str)) {
+      delete strArr[strArr.indexOf(str)];
+      break;
+    }
+  }
+
+  return Number(strArr.join(''));
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
